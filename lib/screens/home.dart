3c079e71c23c55611b76b812e3d9889e93670cd8
Daniel-0507 <davdiu20@epoka.edu.al>
2023-10-04 16:33:18 -0400

    home screen

---
+++ lib/screens/home.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/constants/colors.dart';
+
+class Home extends StatelessWidget{
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    debugShowCheckedModeBanner: false;
+    return Scaffold(
+
+      appBar: AppBar(
+        backgroundColor: tdBGColor,
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+          Icon(Icons.menu, color: tdBlack, size: 30),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset('assets/images/avatar.jpeg')),
+          ),
+        ]),
+
+      ),
+
+
+
+      body: Container(
+        child: Text('This is home screen'),
+      ),
+    );
+  }
+}
